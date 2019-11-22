7b3cd43c2dd723d5c0d600f27de3849a10aa3790
Nikola <nikoladalchevski@gmail.com>
2019-11-22 14:58:50 +0100

    songs finished

---
+++ songs/src/components/SongDetails.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title} Duration: <br /> {song.duration}
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { song: state.SelectedSongReducer };
+};
+export default connect(mapStateToProps)(SongDetail);
