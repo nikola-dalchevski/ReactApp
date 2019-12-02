049e529ce2a969863a682f3fededfd5fcbaba24b
Nikola Dalcevski <nikoladalchevski@gmail.com>
2019-12-02 15:31:07 +0100

    text

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,10 @@
 import jsonPlaceHolderApi from "../API/jsonPlaceHolderApi";
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceHolderApi.get("/post");
+  const response = await jsonPlaceHolderApi.get("/posts");
 
   dispatch({
     type: "FETCH_POSTS",
-    payload: response
+    payload: response.data
   });
 };
