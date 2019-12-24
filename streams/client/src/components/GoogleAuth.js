8268eab2ed0654e9e1678799680f946bac5f0b1a
nikola-dalchevski <nikoladalchevski@gmail.com>
2019-12-24 16:06:23 +0100

    'oauth'

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "7090975415-1g02fopn6th8ht264frhnnt48s6583i4.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed id!</div>;
+    } else {
+      return <div>I m not signed in!</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
